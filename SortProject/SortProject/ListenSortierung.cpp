4865e1e8cd02427c7dde677d6a7c4ca62b93fd6a
AC-BYOD <adrian.cerdeira@viag.ch>
2018-12-07 15:11:07 +0100

    Struktur Person erstellt (nach Vorlage)

--- SortProject/SortProject/ListenSortierung.cpp
+++ SortProject/SortProject/ListenSortierung.cpp
@@ -1,6 +1,15 @@
 #include "stdio.h";
 #include "stdlib.h";
 
+// Struktur Person erstellen: Adrian Cerdeira
+typedef struct Person {
+	char Vorname[40];
+	char Nachname[40];
+	int Jahrgang;
+	struct Person* pNext;
+	struct Person* pPrev;
+} struPerson;
+
 int main() {
 
 }
