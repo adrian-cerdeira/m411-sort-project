8d264e3a2e81e78d741ea3539404a8f19d40677e
Mario <36076382+TDMario@users.noreply.github.com>
2018-12-11 18:21:49 +0100

    Create der Liste implementiert
    + switch Case vorbereitet
    + Bubble Sort Funkion vorbereitet

--- SortProject/SortProject/ListenSortierung.cpp
+++ SortProject/SortProject/ListenSortierung.cpp
@@ -1,5 +1,6 @@
 #include "stdio.h";
 #include "stdlib.h";
+#include "time.h";
 
 // Struktur Person erstellen: Adrian Cerdeira
 typedef struct Person {
@@ -10,8 +11,59 @@ typedef struct Person {
 	struct Person* pPrev;
 } struPerson;
 
+// Main-Funktion erstellen: Mario Forrer und Adrian Cerdeira
 int main() {
-	struPerson *pElement = (struPerson *)malloc(sizeof(struPerson));
 
-	printf("Was möchten Sie tun?: Sortieren(s), Löschen(d), Ausgeben(a)\n");
+//	TODO: String-Dreck reparieren (hate it) 
+	char ansc;
+// TODO: Restliche verlangte Funktionen einbauen.
+	printf("Was mÃ¶chten Sie tun?: Sortieren(s), Liste lÃ¶schen(d), Elemente lÃ¶schen (e), Ausgeben(a), \n");
+	scanf("%c", ansc);
+	switch (ansc)
+	{
+	case 's':
+		// Sort Funktion
+		break;
+	case 'd':
+		// Delete Funktion
+	case 'a':
+		// Print Funktion
+		break;
+	case 'l':
+		// Listenauswahl
+		break;
+	case 'e':
+		//Delete Funktion (Einzelne Elemente)
+		break; 
+	default:
+		break;
+	}
 }
+//Create Funktion erstellen: Mario Forrer
+struPerson* Create(const int Anzahl) {
+	srand((unsigned) time(NULL));
+	struPerson *pStart = NULL;
+	struPerson *pCurrent = NULL;
+	struPerson *pPrevious = NULL;
+	for (int i = 0; i < Anzahl; i++) {
+		pCurrent = (struPerson *)malloc(sizeof(struPerson));
+//		TODO: Speichern von Namen und vornamen.
+		pCurrent->Jahrgang = 1900 + rand() % 118;
+		if (pPrevious == NULL) pStart = pCurrent;
+		else {
+			pCurrent->pPrev = pPrevious;
+			pCurrent->pPrev->pNext = pCurrent;
+		}
+		pPrevious = pCurrent;
+	}
+	return pStart;
+}
+//Bubblesort Funktion erstellen: Adrian Cerdeira
+struPerson* BubbleSort(struPerson* pStart) {
+
+
+
+
+}
+
+//*Sort Funktion erstellen: Mario Forrer
